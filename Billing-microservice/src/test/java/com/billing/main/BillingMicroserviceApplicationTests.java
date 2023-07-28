9137879dec81cc4144d57894e1fbcb3872f92630
Santhosh-172 <santhoshvajiram17@gmail.com>
2023-07-28 18:50:24 +0530

    Added microservices

---
+++ Billing-microservice/src/test/java/com/billing/main/BillingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
